71f7fe59f70ed1feab006d66fc99e72790626ecd
Viktor <95305510+Lurkerrrr@users.noreply.github.com>
2022-10-22 05:37:15 +0300

    Task - LowerToUpper Letters

---
+++ task1.py
@@ -0,0 +1,14 @@
+zdanie1 = "JESTEM STUDENTEM WSIiZ"
+
+# zamienia wielkie litery na małe
+print(zdanie1.swapcase())
+
+zdanie2 = "jestem studentem wsiiz"
+
+# zamienia małe litery na wielkie
+print(zdanie2.swapcase())
+
+zdanie3 = "JeStEm StUdEnTeM WsIiZ"
+
+# zamienia małe litery na wielkie i odwrotnie
+print(zdanie3.swapcase())
